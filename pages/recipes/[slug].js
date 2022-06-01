c7d2d9d470d5ba6f27561ece369914fa5ea5e0bc
Jericho Gragasin <gragasinjericho26@gmail.com>
2022-06-02 02:31:56 +0800

    Migrated Portable Text block props to values props

---
+++ pages/recipes/[slug].js
@@ -0,0 +1,88 @@
+import {
+    sanityClient,
+    urlFor,
+    usePreviewSubscription,
+    PortableText
+} from '../../lib/sanity';
+
+
+const recipeQuery = `*[_type == "recipe" && slug.current == $slug][0] {
+    _id,
+    name,
+    slug,
+    mainImage,
+    ingredient[]{
+        _key,
+        unit,
+        wholeNumber,
+        fraction,
+        ingredient -> {
+            name
+        }
+    },
+    instructions
+}`;
+
+export default function OneRecipe({data}) {
+    const recipe = data.recipe;
+    console.log(recipe.instructions)
+    return(
+        <article className='recipe'>
+            <h1>
+                {recipe.name}
+            </h1>
+            <main className='content'>
+                <img src={urlFor(recipe?.mainImage).url()} alt={recipe.name} />
+                <div className='breakdown'>
+                    <ul className='ingredients'>
+                        {recipe.ingredient?.map((ingredient)=> (
+                            <li className='ingredient' key={ingredient._key}>
+                                {ingredient?.wholeNumber}
+                                {ingredient?.fraction}
+                                
+                                {ingredient?.unit}
+                                <br />
+                                {ingredient?.ingredient?.name}
+                            </li>
+                        ))}
+                    </ul>
+                    <div className='instructions'> 
+                        <PortableText value={recipe?.instructions}/>
+                    </div>
+                </div>
+            </main>
+        </article>
+    )
+}
+
+export async function getStaticPaths() {
+    //access the slugs from the database and creates their own paths based on the current file path its related to
+    const paths = await sanityClient.fetch(
+        `*[_type == "recipe" && defined(slug.current)]{
+            "params": {
+                "slug": slug.current
+            }
+        }`
+    );
+    return {
+        //returns the path for it to be usable
+        paths,
+        fallback: true
+    };
+}
+
+export async function getStaticProps({params}) {
+    const {slug} = params;
+    const recipe = await sanityClient.fetch(
+        recipeQuery,
+        {slug}
+    )
+
+    return {
+        props: {
+            data: {
+                recipe
+            }
+        }
+    };
+}
